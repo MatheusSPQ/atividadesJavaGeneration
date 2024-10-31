b370d084077f88168af52da9cf84eebb33a57563
Matheus <matheuspq11@gmail.com>
2024-10-31 16:25:21 -0300

    correcao nome do metodo

--- atividadeVetoresMatrizes31_10/src/Main.java
+++ atividadeVetoresMatrizes31_10/src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         Atividade01 atividade01 = new Atividade01();
-        //atividade01.posicao();
+        //atividade01.position();
         Atividade02 atividade02 = new Atividade02();
         atividade02.vector();
     }
