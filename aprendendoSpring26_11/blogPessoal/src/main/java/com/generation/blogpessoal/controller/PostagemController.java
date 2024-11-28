f0ce6fd83b5727e910dbed6768e86ff1d6a98ab2
Matheus <matheuspq11@gmail.com>
2024-11-28 14:48:29 -0300

    finalizando atividades 28/11

--- aprendendoSpring26_11/blogPessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ aprendendoSpring26_11/blogPessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,11 +2,15 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/postagens")
@@ -28,4 +32,35 @@ public class PostagemController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("titulo/{titulo}")
+    public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+        return ResponseEntity.ok(postagemRepository.findByTituloIgnoreCase(titulo));
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(postagemRepository.save(postagem));
+    }
+
+    @PutMapping
+    public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+        return postagemRepository.findById(postagem.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                        .body(postagemRepository.save(postagem)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        Optional<Postagem> postagem = postagemRepository.findById(id);
+        if(postagem.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        postagemRepository.deleteById(id);
+    }
 }
